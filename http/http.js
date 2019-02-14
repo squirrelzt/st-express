cea795b91caa93e3fc6b8232416da10b8acd802b
squirrel <squirrelzt@126.com>
2019-02-14 13:21:51 +0800

    Express底层使用http模块

---
+++ http/http.js
@@ -0,0 +1,12 @@
+// Express框架底层使用的是http模块，http模块生成服务器
+const http = require('http');
+
+const app = http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-type': 'text/plain'});
+   response.end('hello world http');
+});
+
+if (!module.parent) {
+    app.listen(3000);
+    console.log("listening on 3000")
+}
